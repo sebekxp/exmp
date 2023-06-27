6423204e0ffe9514fb7831b12cef2530478f3955
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-27 12:28:22 +0200

    Updated page icon and metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,13 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'ExMap',
-  description: 'Exploring the map',
+  title: 'MapMaster',
+  description: 'Embark on a map-expedition like never before',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
