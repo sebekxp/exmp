fcdb55425b48c17009c4e9cff7b47f2016837e36
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-27 02:55:48 +0200

    Implemented logic for selecting appropriate sprites

--- src/app/game/Hero.tsx
+++ src/app/game/Hero.tsx
@@ -1,37 +1,41 @@
 'use client';
 import { useContext, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { useDynamicGameDimensions } from '../hooks/useDynamicGameDimensions';
 import { HeroState } from '../redux/slices/hero';
 import { RootState } from '../redux/store';
 import CanvasContext from './canvasContext';
-import { HERO_CLASSES_MAP, HERO_IMAGE_SIZE, MAP_DIMENSIONS } from './constans';
+import { HERO_SPRITES, NUMBER_OF_SPRITES } from './constants';
 
 type HeroProps = HeroState & ReturnType<typeof mapStateToProps>;
-function Hero({ x, y, id, url, loaded }: HeroProps) {
+
+/**
+ * Component responsible for drawing the character on the canvas based on the loaded character asset.
+ * @component
+ * @param props - The component props.
+ * @param props.x - The x-coordinate of the character.
+ * @param props.y - The y-coordinate of the character.
+ * @param props.id - The ID of the character asset.
+ * @param props.loaded - Indicates whether the character asset is loaded or not.
+ * @param props.currentDirection - The current direction of the character.
+ * @param props.directions - The available directions for the character.
+ */
+function Hero({ x, y, id, loaded, currentDirection, directions }: HeroProps) {
   const ctx = useContext(CanvasContext);
+  const { tileSize } = useDynamicGameDimensions();
 
   useEffect(() => {
     if (!ctx) return;
     if (!loaded) return;
 
-    const { sx, sy } = HERO_CLASSES_MAP['FLAME_SWORDSMAN'].icon;
+    const dx = x * tileSize;
+    const dy = y * tileSize;
+    const img = document.querySelector(`#${id}`) as CanvasImageSource;
+    const spriteIndex = directions[currentDirection] % NUMBER_OF_SPRITES;
+    const { sx, sy, w, h } = HERO_SPRITES[spriteIndex][currentDirection];
 
-    // const image = new window.Image() as HTMLImageElement;
-    // image.src = url;
-
-    ctx.drawImage(
-      // @ts-ignore
-      document.querySelector(`#${id}`),
-      sx,
-      sy,
-      HERO_IMAGE_SIZE + MAP_DIMENSIONS.TILE_SIZE,
-      HERO_IMAGE_SIZE + MAP_DIMENSIONS.TILE_SIZE,
-      x * MAP_DIMENSIONS.TILE_SIZE,
-      y * MAP_DIMENSIONS.TILE_SIZE,
-      HERO_IMAGE_SIZE,
-      HERO_IMAGE_SIZE,
-    );
-  }, [ctx, x, y, loaded, id]);
+    ctx.drawImage(img, sx, sy, w, h, dx, dy, w, h);
+  }, [ctx, x, y, loaded, id, currentDirection, tileSize]);
 
   return null;
 }
@@ -39,6 +43,7 @@ function Hero({ x, y, id, url, loaded }: HeroProps) {
 const mapStateToProps = (state: RootState) => ({
   ...state.hero,
   ...state.assets.HeroAsset,
+  ...state.gameStatus,
 });
 
 export default connect(mapStateToProps)(Hero);
