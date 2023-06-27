8d2271ff31dd7577d5bc37656812c67d21492e64
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-27 02:58:09 +0200

    Aded docs for useCanvas hook

--- src/app/hooks/useCanvas.ts
+++ src/app/hooks/useCanvas.ts
@@ -1,5 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 
+/**
+ * Custom hook for managing a canvas element and its 2D rendering context.
+ * @returns An array containing a ref to the canvas element and the 2D rendering context.
+ */
 export function useCanvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
