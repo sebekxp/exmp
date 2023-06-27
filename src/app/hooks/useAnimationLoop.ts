6c3b6925bfb3f2cc1e2e2033e145d9016c768d46
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-27 02:57:33 +0200

    Simplified animation loop

--- src/app/hooks/useAnimationLoop.ts
+++ src/app/hooks/useAnimationLoop.ts
@@ -1,33 +1,23 @@
-import { Dispatch, SetStateAction, useCallback, useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 /**
- * Hook that creates an animation loop using requestAnimationFrame and calls the callback functions on each frame.
- * @param isUpdateRequired - Determines whether an update is required for the animation loop.
- * @param setIsUpdateRequired - Callback function to set the update requirement.
- * @param setIsVisible - Callback function to set the visibility state.
+ * Hook that creates an animation loop using requestAnimationFrame.
  */
-export function useAnimationLoop(
-  isUpdateRequired: boolean,
-  setIsUpdateRequired: Dispatch<SetStateAction<boolean>>,
-  setIsVisible: Dispatch<SetStateAction<boolean>>,
-) {
-  const loopRef = useRef<number | null>(null);
-  const shouldStopRef = useRef(false);
-
-  const loop = useCallback(() => {
-    if (!isUpdateRequired) return;
-    setIsVisible(true);
-    setIsUpdateRequired(false);
-    loopRef.current = requestAnimationFrame(loop);
-  }, [isUpdateRequired, setIsVisible, setIsUpdateRequired]);
+export function useAnimationLoop() {
+  /**
+   * Callback function for the animation loop.
+   */
+  const loop = () => {
+    requestAnimationFrame(loop);
+  };
 
   useEffect(() => {
-    loopRef.current = requestAnimationFrame(loop);
+    // Start the animation loop by requesting the first animation frame
+    const animationFrame = requestAnimationFrame(loop);
+
+    // Cleanup function to cancel the animation frame when the component unmounts or the dependencies change
     return () => {
-      shouldStopRef.current = true;
-      if (loopRef.current) {
-        cancelAnimationFrame(loopRef.current);
-      }
+      cancelAnimationFrame(animationFrame);
     };
-  }, [loop]);
+  }, []);
 }
