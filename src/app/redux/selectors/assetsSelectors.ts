5edf775a9cb2605f51015df0bd45a93af427af0c
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-27 12:46:38 +0200

    Extended JSDocs with description and added new doc

--- src/app/redux/selectors/assetsSelectors.ts
+++ src/app/redux/selectors/assetsSelectors.ts
@@ -1,5 +1,11 @@
 import { RootState } from '../store';
 
+/**
+ * Checks whether all assets are loaded.
+ *
+ * @param state - The root state object.
+ * @returns Returns true if all assets are loaded, otherwise returns false.
+ */
 export function areAllAssetsLoadedSelector(state: RootState) {
   return Array.from(Object.values(state.assets)).every((asset) => asset.loaded !== false);
 }
--- src/app/utils/collision.ts
+++ src/app/utils/collision.ts
@@ -1,10 +1,17 @@
 /**
  * Checks if the given coordinates are at the edge of the map.
+ *
  * @param x - The x-coordinate.
  * @param y - The y-coordinate.
  * @param rows - The number of rows in the map.
  * @param columns - The number of columns in the map.
  * @returns Returns true if the coordinates are at the edge of the map, false otherwise.
+ *
+ * @description
+ * The function checks whether the given coordinates (x, y) are at the edge of the map.
+ * The function assumes the use of a frame or border around the map, so it applies some
+ * margin. This margin is applied to ensure proper visual alignment and avoid cutting
+ * off rows and columns due to the frame.
  */
 function isMapEdge(x: number, y: number, rows: number, columns: number) {
   return x < 2 || x >= rows - 3 || y < 2 || y >= columns - 5;
