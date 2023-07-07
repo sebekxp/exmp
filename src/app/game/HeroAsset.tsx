e25f91c5a264099f2e884a1eb358b7f5d2727971
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-07-07 18:31:29 +0200

    Deleted unused refs and changed incorrect asset url

--- src/app/game/HeroAsset.tsx
+++ src/app/game/HeroAsset.tsx
@@ -1,22 +1,19 @@
 'use client';
 import Image from 'next/image';
-import { useRef } from 'react';
 import { useAppDispatch } from '../hooks/redux';
 import { loadAsset } from '../redux/slices/assets';
 
 export function HeroAsset() {
   const dispatch = useAppDispatch();
-  const imgRef = useRef<HTMLImageElement | null>(null);
   const id = 'hero';
   return (
     <Image
       id={id}
-      className="imgaes-buffer invisible z-10"
-      ref={imgRef}
+      className="images-buffer invisible z-10"
       src={'/sprites.png'}
       alt="Hero"
       fill={true}
-      onLoad={() => dispatch(loadAsset({ key: 'HeroAsset', url: '/hero.webp', id: id }))}
+      onLoad={() => dispatch(loadAsset({ key: 'HeroAsset', url: '/sprites.png', id: id }))}
     />
   );
 }
--- src/app/game/MapAsset.tsx
+++ src/app/game/MapAsset.tsx
@@ -1,19 +1,16 @@
 'use client';
 import Image from 'next/image';
-import { useRef } from 'react';
 import { useAppDispatch } from '../hooks/redux';
 import { loadAsset } from '../redux/slices/assets';
 
 export function MapAsset() {
   const dispatch = useAppDispatch();
-  const imgRef = useRef<HTMLImageElement | null>(null);
   const id = 'map';
   return (
     <div className="relative h-screen">
       <Image
         className="relative p-5"
         id={id}
-        ref={imgRef}
         src="/map.png"
         alt="Map"
         fill={true}
